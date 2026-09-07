0f2407da06d38d4111e5130033e0238bc067bd2d
BanyuBaningX7 <baningnugroho@gmail.com>
2026-09-07 12:32:49 +0700

    Selesai migration users dengan kolom role

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            // Kolom role penyesuaian Use Case Diagram
+            $table->enum('role', ['super_admin', 'admin', 'siswa', 'guru'])->default('siswa');
             $table->rememberToken();
             $table->timestamps();
         });
